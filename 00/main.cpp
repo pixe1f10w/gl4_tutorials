ab75590cbc6ca7b7e89921e2bb424e2d0fbce6ec
Ilia Zhirov <izhirov@gmail.com>
2017-09-03 12:09:48 +0500

    ready to draw something

--- 00/main.cpp
+++ 00/main.cpp
@@ -33,7 +33,64 @@ int main () {
   glEnable(GL_DEPTH_TEST); // enable depth-testing
   glDepthFunc(GL_LESS); // depth-testing interprets a smaller value as "closer"
 
-  /* OTHER STUFF GOES HERE NEXT */
+  // geometry definition
+  const GLfloat points[] = {
+    .0f, .5f, .0f,
+    .5f, -.5f, .0f,
+    -.5f, -.5f, .0f
+  };
+
+  // vertex buffer object
+  GLuint vbo = 0;
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+
+  // vertex array object
+  GLuint vao = 0;
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
+  glEnableVertexAttribArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+  // shaders definitions
+  const char* vertex_shader =
+    "#version 400\n"
+    "in vec3 vp;"
+    "void main() {"
+    "  gl_Position = vec4(vp, 1.0);"
+    "}";
+
+  GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vs, 1, &vertex_shader, nullptr);
+  glCompileShader(vs);
+
+  const char* fragment_shader =
+    "#version 400\n"
+    "out vec4 frag_colour;"
+    "void main() {"
+    "  frag_colour = vec4(0.5, 0.0, 0.5, 1.0);"
+    "}";
+
+  GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fs, 1, &fragment_shader, nullptr);
+  glCompileShader(fs);
+
+  // shader program definition
+  GLuint shader_program = glCreateProgram();
+  glAttachShader(shader_program, fs);
+  glAttachShader(shader_program, vs);
+  glLinkProgram(shader_program);
+
+  // draw loop
+  while (!glfwWindowShouldClose(window)) {
+    // wipe the drawing surface
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glfwPollEvents();
+    glfwSwapBuffers(window);
+  }
 
   // close GL context and any other GLFW resources
   glfwTerminate();
